844034649159bb8048c89eb13780d0139ad09e4f
sarapappila <sara.pappila@metropolia.fi>
2024-04-08 10:06:51 +0300

    Tehdään tehtävä express.md

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from 'express';
 
 const app = express();
+app.use('/public', express.static('public'));
 
 app.get('/api/v1/cat', (req, res) => {
     const cat = {
         cat_id: 1,
         name: 'Whiskers',
-        birthdate: '2022-01-01',
+        birthdate: '14-01-2002',
         weight: 5,
-        owner: 'John Doe',
-        image: 'https://loremflickr.com/320/240/cat'
+        owner: 'Sara',
+        image: 'https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNjM2LWltYWdlLTAxLXJtNTAzXzMtbDBqOXFrNnEucG5n.png'
     };
     res.json(cat);
 });
