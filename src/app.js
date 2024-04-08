5c4e573f7e97b7588df52f75d5907b52c84eba85
sarapappila <sara.pappila@metropolia.fi>
2024-04-08 10:19:21 +0300

    Lisätään pohjaa

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+app.use('/public', express.static('public'));
+
+app.get('/api/v1/cat', (req, res) => {
+    const cat = {
+        cat_id: 1,
+        name: 'Whiskers',
+        birthdate: '14-01-2002',
+        weight: 5,
+        owner: 'Sara',
+        image: 'https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNjM2LWltYWdlLTAxLXJtNTAzXzMtbDBqOXFrNnEucG5n.png'
+    };
+    res.json(cat);
+});
+
+export default app;
