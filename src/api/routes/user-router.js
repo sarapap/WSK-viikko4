2f2a927722504f4b113d8ce515481e40adefa548
sarapappila <sara.pappila@metropolia.fi>
2024-04-12 15:31:31 +0300

    Lisätään

--- src/api/routes/user-router.js
+++ src/api/routes/user-router.js
@@ -8,7 +8,14 @@ import {
 } from '../controllers/user-controller.js';
 
 const userRouter = express.Router();
-userRouter.route('/').get(getUser).post(postUser);
+userRouter.route('/')
+    .get(getUser)
+    .post(
+        body('email').trim().isEmail(),
+        body('username').trim().isLength({ min: 3, max: 20 }).isAlphanumeric(),
+        body('password').trim().isLength({ min: 8 }),
+        validationErrors,
+        postUser);
 
 userRouter.route('/:id').get(getUserById).put(putUser).delete(deleteUser);
 
