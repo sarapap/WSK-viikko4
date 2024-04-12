04212b76000fe9023f9f7c0d0a88067505cf6ebb
sarapappila <sara.pappila@metropolia.fi>
2024-04-12 13:22:57 +0300

    Lisätään

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import api from './api/index.js';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
